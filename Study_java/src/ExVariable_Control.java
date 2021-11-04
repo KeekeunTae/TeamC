4d29d389eada9cd7ca874bd1d62cd31a094110eb
명운 <48548596+WAWWW121@users.noreply.github.com>
2021-11-04 17:52:19 +0900

    20211104_제어문까지

---
+++ Study_java/src/ExVariable_Control.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ExVariable_Control {
+public static void main(String[] args) {
+	//..?..?..?..?..?
+	//?..?..?..?..?..
+//	int a= 5;
+//	System.out.println(a);
+//	String b = a+"";
+//	a= Integer.parseInt(b);
+//	char chr = '1';
+//	a = Integer.parseInt(chr+"");
+//	b=a+"";
+//	
+	Scanner sc = new Scanner(System.in);
+	String a = sc.nextLine(); 
+	if(a.equals("A")) {
+		System.out.println("A맞음");
+	}else {
+		System.out.println("아님");
+	}
+	
+	
+	
+}
+}
---
+++ Study_java/src/ExVariable_Control2.java
@@ -0,0 +1,20 @@
+
+public class ExVariable_Control2 {
+	
+public static void main(String[] args) {
+	int num1 =0;
+	if(true) {
+		int num2 =1;
+		System.out.println("1");
+		System.out.println(num2);
+	}else if(true){
+		System.out.println("2");
+		System.out.println(num1);
+	}else if(true) {
+		System.out.println("3");
+	}else {
+		System.out.println("끝");
+	}
+	
+}
+}
